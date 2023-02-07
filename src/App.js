8b87fb3c3a1941c4f0e0624c8668a9f77e37a989
Omar Mohammed <omarmohammedkhedor@yahoo.com>
2023-02-07 14:03:56 +0300

    fix home page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/whatsapp-react" element={<Home />} />
+        <Route path="/whatsapp-react/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
